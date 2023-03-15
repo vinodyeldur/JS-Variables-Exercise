fe5eaa89bc6f1ad1d92b15399b8f024f65ee4b76
Vinod1999 <109039225+vinodyeldur@users.noreply.github.com>
2023-03-15 04:23:49 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+
+    var a = "3";
+    var b = "8";
+    
+/***********Do not change the code above 👆*******/
+
+    var c = a;
+    a = b;
+    b = c;
+
+
+    console.log("a is " + a);
+    console.log("b is " + b);
+
